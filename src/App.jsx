187061db349bfc62d671aeb9a48b59a278700269
joshua <iuttn123@gmail.com>
2025-02-21 12:15:04 +0900

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import TableRow from "./container/TableRow";
 
 function App() {
+  const css = { backgroundColor: "red", color: "white" };
+
+  const arr = [
+    { tableData: { data: ["2017년 5월 28일"] }, header: { headerName: "창단일", ...css } },
+    { tableData: { data: ["머한민국"] }, header: { headerName: "소재지", ...css } },
+    { tableData: { data: ["㈜에이디이스포츠"] }, header: { headerName: "법인명", ...css } },
+    { tableData: { data: ["어비스컴퍼니"] }, header: { headerName: "모기업", ...css } },
+    { tableData: { data: ["리그 오브 레전드", "배틀그라운드", "발로란트", "레인보우 식스 시즈"] }, header: { headerName: "종목", ...css } },
+  ];
+
   return (
     <tbody>
-      <TableRow tableData={{ data: ["2017년 5월 28일"] }} header={{ headerName: "창단일", backgroundColor: "black", color: "white" }} />
-      <TableRow tableData={{ data: ["머한민국"] }} header={{ headerName: "소재지", backgroundColor: "black", color: "white" }} />
-      <TableRow tableData={{ data: ["㈜에이디이스포츠"] }} header={{ headerName: "법인명", backgroundColor: "black", color: "white" }} />
-      <TableRow tableData={{ data: ["어비스컴퍼니"] }} header={{ headerName: "모기업", backgroundColor: "black", color: "white" }} />
-      <TableRow tableData={{ data: ["리그 오브 레전드", "배틀그라운드", "발로란트", "레인보우 식스 시즈"] }} header={{ headerName: "종목", backgroundColor: "black", color: "white" }} />
+      {arr.map((v) => (
+        <TableRow {...v} />
+      ))}
     </tbody>
   );
 }
